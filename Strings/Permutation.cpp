14927e8e4643bea2c0ccd63b55beb0f22b497a72
Ashis Shrestha <shrestha7.np@gmail.com>
2025-06-05 22:25:40 -0500

    Permutation in String

---
+++ Strings/Permutation.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+bool isFreqSame (int freq1[], int freq2[])
+{
+    for (int i=0; i<26; i++){
+        if (freq1[i]!=freq2[i]) return false;
+    }
+    return true;
+}
+
+bool checkInclusion(string s1, string s2)
+{
+    int freq[26] = {0};
+    
+    for(int i=0; i<s1.size(); i++){
+        freq[s1[i]-'a']++;
+    }
+
+    int windSize = s1.size();
+    for(int i=0; i<s2.size(); i++){
+        int windIdx = 0, idx = i;
+        int windFreq[26] = {0};
+
+        while(windIdx < windSize && idx < s2.size()){
+            windFreq[s2[idx]-'a']++;
+            windIdx++;
+            idx++;
+        }
+
+        if(isFreqSame(freq,windFreq)){
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    // string s1 = "ab", s2="eidbaooo";
+    string s1 = "ab", s2="eidbooaooo";
+    if (checkInclusion(s1,s2)) cout << "Permutation Found" << endl;
+    else cout << "Permutation not found" << endl;
+
+    return 0;
+}
